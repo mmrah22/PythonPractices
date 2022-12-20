6512b7a762da27fe2ec113180f9f4567441eec1d
mmrah22 <121082011+mmrah22@users.noreply.github.com>
2022-12-20 18:29:17 -0500

    Update pnpr1.py

--- .github/workflows/pnpr1.py
+++ .github/workflows/pnpr1.py
@@ -1,1 +1,7 @@
+import os
+
 print("Hello from Earth.")
+token = os.environ.get("THIS_SECRET_QUOTE")
+if not token:
+  raise RuntimeError("Access Denied. Gg.")
+print("The env var was found. Good.")
